6a8da0a76e331c629131817868930e30a176b89e
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-01-20 11:36:42 +0330

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from './components/header';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>League of Legends Memory Game</h1>
+    </header>
+  );
+};
+
+export default Header;
