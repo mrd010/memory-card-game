dea8ee94c8fce2dc65cb6f26a85045c71bc0bb98
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-01-24 12:13:45 +0330

    Remove image src variable

--- src/components/ChampImage.jsx
+++ src/components/ChampImage.jsx
@@ -11,17 +11,13 @@ const ChampImage = ({ champName }) => {
   }, [isLoaded]);
 
   const imageLoader = !isLoaded ? <div className="image-loader"></div> : null;
-  const imgSrc =
-    champName !== ''
-      ? `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${capitalize(champName)}_0.jpg`
-      : '';
 
   return (
     <>
       <img
         className="champ-picture"
         ref={r}
-        src={imgSrc}
+        src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${capitalize(champName)}_0.jpg`}
         alt={champName}
         style={{ opacity: '0' }}
         onLoad={() => {
